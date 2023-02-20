121b611d30ca75c439e4f44b700ce80ea90bc23a
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-02-20 02:29:47 +0100

    Optimize setting uniforms

--- src/main.cpp
+++ src/main.cpp
@@ -15,28 +15,31 @@ int main() {
 	ImGui::GetIO().IniFilename = nullptr;
 	auto vert = gfx::quad_vertex_shader();
 	board_renderer board{vert};
+	board.use();
+	board.set_white_color(view.light_color);
+	board.set_black_color(view.dark_color);
+	board.set_flipped(view.flipped);
 	vert.~shader();
 	gfx::matrix transform;
 	while(ctx.update([&](auto width, auto height) {
+		auto w = width > height ? static_cast<float>(height) / static_cast<float>(width) : 1;
+		auto h = height > width ? static_cast<float>(width) / static_cast<float>(height) : 1;
 		gfx::imgui::frame _{};
+		board.use();
 		if(ImGui::BeginMainMenuBar()) {
 			if(ImGui::BeginMenu("View")) {
 				ImGui::Text("Light Color:"); ImGui::SameLine();
-				ImGui::ColorEdit3("Light Color:", view.light_color, color_edit_flags);
+				if(ImGui::ColorEdit3("Light Color:", view.light_color, color_edit_flags))
+					board.set_white_color(view.light_color);
 				ImGui::Text("Dark Color: "); ImGui::SameLine();
-				ImGui::ColorEdit3("Dark Color:", view.dark_color, color_edit_flags);
+				if(ImGui::ColorEdit3("Dark Color:", view.dark_color, color_edit_flags))
+					board.set_black_color(view.dark_color);
 				if(ImGui::Button("Flip"))
-					view.flipped = !view.flipped;
+					board.set_flipped(view.flipped = !view.flipped);
 				ImGui::EndMenu();
 			}
 			ImGui::EndMainMenuBar();
 		}
-		auto w = width > height ? static_cast<float>(height) / static_cast<float>(width) : 1;
-		auto h = height > width ? static_cast<float>(width) / static_cast<float>(height) : 1;
-		board.use();
-		board.set_white_color(view.light_color);
-		board.set_black_color(view.dark_color);
-		board.set_flipped(view.flipped);
 		gfx::set_transformation(transform |
 			gfx::identity |
 			gfx::scale(w, h) |
