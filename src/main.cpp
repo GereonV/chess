8caf2a4720bb785df5f7d76ba8098d812c38d335
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-02-21 18:17:48 +0100

    Move ImGui-menu logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,41 @@
 #include <cstdlib> // std::getenv()
+#include "io.hpp"
+#include "menu.hpp"
 #include "rendering.hpp"
-#include "settings.hpp"
 
-inline board_view view;
-
-inline constexpr int color_edit_flags = ImGuiColorEditFlags_NoDragDrop | ImGuiColorEditFlags_NoLabel | ImGuiColorEditFlags_NoInputs;
+// returns if board.use()
+inline bool update_settings(board_settings & bs, board_renderer const & board) noexcept {
+	auto change = menu(bs);
+	if(change == settings_change::none)
+		return false;
+	write_config(config{bs});
+	board.use();
+	if(change == settings_change::flip)
+		board.set_flipped(bs.flipped);
+	else if(change == settings_change::light_color)
+		board.set_light_color(bs.light_color);
+	else
+		board.set_dark_color(bs.dark_color);
+	return true;
+}
 
 int main() {
-	auto s = read_settings();
-	view.light_color = s.light_color;
-	view.dark_color = s.dark_color;
+	auto config = read_config();
+	board_settings b_settings{config.board}; // default flipped
 	gfx::context ctx{"Schach", 1280, 720};
 	ctx.clear_to(.12f, .12f, .12f);
 	gfx::imgui::context imctx{ctx.window};
 	ImGui::GetIO().IniFilename = nullptr;
 	auto vert = gfx::quad_vertex_shader();
-	board_renderer board{vert, view};
+	board_renderer board{vert, b_settings};
 	vert.~shader();
-	render_settings settings;
+	render_settings r_settings; // default size
 	while(ctx.update([&](auto width, auto height) {
+		r_settings.scale_for(width, height);
 		gfx::imgui::frame _{};
-		settings.scale_for(width, height);
-		board.use();
-		if(ImGui::BeginMainMenuBar()) {
-			if(ImGui::BeginMenu("View")) {
-				ImGui::Text("Light Color:"); ImGui::SameLine();
-				if(ImGui::ColorEdit3("Light Color:", view.light_color, color_edit_flags))
-					board.set_light_color(view.light_color);
-				ImGui::Text("Dark Color: "); ImGui::SameLine();
-				if(ImGui::ColorEdit3("Dark Color:", view.dark_color, color_edit_flags))
-					board.set_dark_color(view.dark_color);
-				if(ImGui::Button("Flip"))
-					board.set_flipped(view.flipped = !view.flipped);
-				ImGui::EndMenu();
-			}
-			ImGui::EndMainMenuBar();
-		}
-		render(settings, board);
+		if(!update_settings(b_settings, board))
+			board.use();
+		render(r_settings);
 	}));
 }
 
---
+++ src/menu.hpp
@@ -0,0 +1,36 @@
+#ifndef CHESS_MENU_HPP
+#define CHESS_MENU_HPP
+
+#include <GFX/opengl/loader.hpp>
+
+inline constexpr int color_edit_flags = ImGuiColorEditFlags_NoDragDrop | ImGuiColorEditFlags_NoLabel | ImGuiColorEditFlags_NoInputs;
+
+enum class settings_change {
+	none,
+	light_color,
+	dark_color,
+	flip
+};
+
+[[nodiscard]] inline settings_change menu(board_settings & s) {
+	auto change = settings_change::none;
+	if(ImGui::BeginMainMenuBar()) {
+		if(ImGui::BeginMenu("View")) {
+			ImGui::Text("Light Color:"); ImGui::SameLine();
+			if(ImGui::ColorEdit3("Light Color:", s.light_color, color_edit_flags))
+				change = settings_change::light_color;
+			ImGui::Text("Dark Color: "); ImGui::SameLine();
+			if(ImGui::ColorEdit3("Dark Color:", s.dark_color, color_edit_flags))
+				change = settings_change::dark_color;
+			if(ImGui::Button("Flip")) {
+				s.flipped = !s.flipped;
+				change = settings_change::flip;
+			}
+			ImGui::EndMenu();
+		}
+		ImGui::EndMainMenuBar();
+	}
+	return change;
+}
+
+#endif // CHESS_MENU_HPP
