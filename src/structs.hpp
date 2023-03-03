d42992dac7f631dced2db751f93f9b7c15a51aa9
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-03-03 15:45:45 +0100

    Restructure code by moving to sensible locations

--- src/structs.hpp
+++ src/structs.hpp
@@ -14,9 +14,32 @@ struct board_settings : board_config {
 	bool flipped{false};
 };
 
-// config file
-struct config {
-	board_config board;
+struct color {
+private:
+	using      data = float[3];
+	using       ref = data       &;
+	using const_ref = data const &;
+public:
+	color() = default;
+	constexpr color(float r, float g, float b) : _rgb{r, g, b} {}
+	constexpr void r(float r) noexcept { _rgb[0] = r; }
+	constexpr void g(float g) noexcept { _rgb[1] = g; }
+	constexpr void b(float b) noexcept { _rgb[2] = b; }
+	constexpr void set(float r, float g, float b) noexcept {
+		_rgb[0] = r;
+		_rgb[1] = g;
+		_rgb[2] = b;
+	}
+
+	constexpr float r() const noexcept { return _rgb[0]; }
+	constexpr float g() const noexcept { return _rgb[1]; }
+	constexpr float b() const noexcept { return _rgb[2]; }
+	constexpr       ref rgb()       noexcept { return _rgb; }
+	constexpr const_ref rgb() const noexcept { return _rgb; }
+	constexpr operator       ref()       noexcept { return _rgb; }
+	constexpr operator const_ref() const noexcept { return _rgb; }
+private:
+	data _rgb;
 };
 
 #endif // CHESS_STRUCTS_HPP
